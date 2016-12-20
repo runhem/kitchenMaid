ad2b171ced8d969ede68e54d56d6fd0b915b05ab
fridaelkund <friekl@kth.se>
2016-12-20 13:15:58 +0100

    Revert "finish when end of recipe"

--- src/iristk/app/kitchenMaid/Instruction.java
+++ src/iristk/app/kitchenMaid/Instruction.java
@@ -15,12 +15,15 @@ import java.util.List;
 
 import iristk.util.RandomList;
 
-//Instruction class
+//Question class med id, question, category, answers, correct som attribut nu. 
+//Vi kan ändra dessa sen till något annat 
+//kolla hur det kan matchas med textfilen eller om det bestäms i Question nedan?
 public class Instruction {
 
-	//Declaration of variables
 	private  String id;
 	private String question;
+	// private String[] answers = new String[4];
+	// private int correct;
 	private String action;
 	private String ingredient;
 	private String measure;
@@ -31,14 +34,28 @@ public class Instruction {
 	
 	//Getting different parts of the intruction lines and storing them in the right variables
 	public Instruction(String id, String[] cols) {
-	
-		//Instruction id, to know which one we're at 
+		
+		//Frågeid
 		this.id = id;
 		
-		// Fetching action - there is always an action in the instructions
+		// Fetches action - there is always an action in the instructions
 		action = cols[0].trim();
 		
-		//If there are measurement, ingredient or container on line we create them here
+		//skapa lista som heter order, endast till för svarsalternativen 
+		//List<Integer> order = Arrays.asList(new Integer[]{0, 1, 2, 3});
+		
+		//Blanda svarens ordning - kommer vi ej behöva ?
+//		RandomList.shuffle(order);
+//		
+//		answers[order.get(0)] = cols[0].trim();
+//		answers[order.get(1)] = cols[0].trim();
+//		answers[order.get(2)] = cols[0].trim();
+//		answers[order.get(3)] = cols[0].trim();
+//		
+		//Vilket som är rätt svar - kommer vi ej behöva? 
+		//correct = order.get(0);
+		
+		//If there are measurement, ingredient or container on line we create these
 		if (cols.length > 1) {
 			measure = cols[1].trim();
 		}
@@ -50,7 +67,7 @@ public class Instruction {
 		}
 	}
 	
-	//Returns instruction on different formats depending on what it contains
+	//Returns instruction on different formats depending on what it contains *** CHANGE NAME TO getFullInstruction() ***  
 	public String getFullInstruction() {
 		returnstring = action;
 		if(measure != null){
@@ -65,7 +82,12 @@ public class Instruction {
 		
 		return  returnstring;
 	}
-
+	
+	//Kan vi änvända fast för någonting annat sen, typ 'repeat' eller någon följdfråga
+//	public Object getOptions() {
+//		return answers[0] + ", " + answers[1] + ", " + answers[2] + ", or " + answers[3];
+//	}
+	
 	
 	// *** Look into how we should change this *** 
 //	public String getGrammar() {
@@ -80,7 +102,18 @@ public class Instruction {
 //		return grammar.toString();
 //	}
 
-	//Returns the id
+
+	//Användas för confirmation 'ready'/'not ready'
+//	public String getCorrectAnswer() {
+//		return answers[correct];
+//	}
+
+	//Feedback för att vi ska gå vidare typ? 
+//	public boolean isCorrect(Object answer) {
+//		return answer != null && answer.toString().equals("answer" + correct);
+//	}
+
+	//För att veta vilken fråga vi är på - eller vilket steg för oss sen
 	public String getId() {
 		return id;
 	}
